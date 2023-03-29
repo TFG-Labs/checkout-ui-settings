a5dca01992b743f9388bdb001f82a6fa480ec3c1
John Simms <johnbsimms@gmail.com>
2023-03-29 09:54:25 +0200

    ShippintTabs component

--- checkout-ui-custom/src/partials/ShippingTabs.js
+++ checkout-ui-custom/src/partials/ShippingTabs.js
@@ -1,6 +1,6 @@
 const ShippingTabs = () => `
       <div id="shipping-tabs--tabs">
-        <button id="shipping-tab-deliver>Deliver</button>
+        <button id="shipping-tab-deliver">Deliver</button>
         <button id="shipping-tab-collect">Collect</button>
       </div>
 `;
