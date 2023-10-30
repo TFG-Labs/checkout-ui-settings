8cd16496d706ae28922c677098333e0e2a116154
John Simms <johnbsimms@gmail.com>
2023-10-30 09:41:51 +0200

    Add correctCoords function

--- checkout-ui-custom/src/utils/isInSouthAfrica.js
+++ checkout-ui-custom/src/utils/isInSouthAfrica.js
@@ -1,12 +1,24 @@
+// @ts-nocheck
 /**
  * isInSouthAfrica
  * Validate coordinates to ensure that they are in South Africa.
  * @param {[lnt, lat]} coordinates
  * @returns
  */
-const isInSouthAfrica = (coordinates) => {
+export const isInSouthAfrica = (coordinates) => {
   const [lng, lat] = coordinates;
-  return lng >= 16.3440 && lng <= 32.8301 && lat >= -34.8191 && lat <= -22.1277;
+  return lng >= 16.344 && lng <= 32.8301 && lat >= -34.8191 && lat <= -22.1277;
+};
+
+export const correctCoords = (coords) => {
+  if (!coords || coords.length < 2) return ['', ''];
+
+  const [lng, lat] = coords;
+
+  if (isInSouthAfrica([lng, lat])) return [lng, lat];
+  if (isInSouthAfrica([lat, lng])) return [lat, lng];
+
+  return ['', ''];
 };
 
 export default isInSouthAfrica;
