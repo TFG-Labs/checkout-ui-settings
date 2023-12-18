4098996d3abec0e2321f0b8deddef2ec3c999b53
John Simms <johnbsimms@gmail.com>
2023-12-18 13:58:39 +0200

    Set up HTML button for Delivery view.

---
+++ checkout-ui-custom/src/partials/Deliver/DeliverySummaryCollectButton.js
@@ -0,0 +1,8 @@
+/* eslint-disable arrow-body-style */
+const DeliverySummaryCollectButton = () => {
+  return `<a id="injected-collect-button" href="#/shipping">
+  <div class="vtex-omnishipping-1-x-deliveryChannelsWrapper shipping-method-wrapper is-active vtex-omnishipping-1-x-isActive shipping-valid-postal-code shipping-has-pickups"><div class="shipping-method-toggle vtex-omnishipping-1-x-deliveryChannelsToggle shipping-method-toggle-delivery"></div><span class="shp-method-option vtex-omnishipping-1-x-deliveryChannelsOption shp-method-option-active vtex-omnishipping-1-x-deliveryOptionActive" id="shipping-option-delivery" type="button"><span class="shp-method-option-text">Ship</span><span class="shp-method-option-complement vtex-omnishipping-1-x-deliveryChannelsComplement">to address</span></span><span class="shp-method-option vtex-omnishipping-1-x-deliveryChannelsOption shp-method-option-inactive vtex-omnishipping-1-x-deliveryOptionInactive" id="shipping-option-pickup-in-point" type="button"><span class="shp-method-option-text">Pick up</span><span class="shp-method-option-complement vtex-omnishipping-1-x-deliveryChannelsComplement">in store</span></span></div>
+  </a>`;
+};
+
+export default DeliverySummaryCollectButton;
