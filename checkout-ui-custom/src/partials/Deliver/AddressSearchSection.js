c1569c482c2760e28cb2a09b8f0dac92a4666157
Shaheen Karodia <karodia.shaheen@gmail.com>
2024-08-01 21:36:49 +0200

    Split Address Search Section into different components

--- checkout-ui-custom/src/partials/Deliver/AddressSearchSection.js
+++ checkout-ui-custom/src/partials/Deliver/AddressSearchSection.js
@@ -1,23 +1,31 @@
 import AddressSearch from './AddressSearch';
 
+const Heading = () => /* html */ `
+  <div class="bash--heading">
+    <h3>Add a new delivery address</h3>
+    <a href='#' data-view='select-address' id='back-button-select-address'>&lt; Back</a>
+  </div>
+`;
+
+const ManualSearchCTA = () => /* html */ `
+  <p style="font-size: 12px; margin: 16px 0" id="type-your-address-above">
+    Type your address above or 
+    <a 
+      href="" id="link-manual-address-entry"
+      data-view="address-form"
+      onClick="document.getElementById('bash--input-street').focus()"
+      style="text-decoration: underline" 
+    >enter it manually</a>.
+  </p>
+`;
+
 const AddressSearchSection = () => /* html */ `
     <section class="bash--delivery-view" data-section="address-search">
-      <div class="bash--heading">
-        <h3>Add a new delivery address</h3>
-        <a href='#' data-view='select-address' id='back-button-select-address'>&lt; Back</a>
-      </div>
+      ${Heading()}
       <div class="address-search-field-container" id="address-search-field-container">
           ${AddressSearch()} 
       </div>
-      <p style="font-size: 12px; margin: 16px 0" id="type-your-address-above">
-        Type your address above or 
-        <a 
-          href="" id="link-manual-address-entry"
-          data-view="address-form"
-          onClick="document.getElementById('bash--input-street').focus()"
-          style="text-decoration: underline" 
-        >enter it manually</a>.
-      </p>
+      ${ManualSearchCTA()}
     </section>
 `;
 
