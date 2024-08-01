3eda85c9d53a543c45e97455b2f4fee3fce9725a
Segopotso Makhutja <leketi.s@gmail.com>
2024-08-01 15:30:06 +0200

    update address list after deleting

--- checkout-ui-custom/src/utils/deleteAddress.js
+++ checkout-ui-custom/src/utils/deleteAddress.js
@@ -1,6 +1,8 @@
+import AddressListing from '../partials/Deliver/AddressListing';
 import { showAlertBox } from '../partials/Deliver/utils';
 import {
   getAddressByName,
+  getAddresses,
   removeAddressFromDB,
   removeAddressFromMasterData,
   removeAddressFromOrderForm,
@@ -27,8 +29,16 @@ const handleDeleteAddress = async (addressName) => {
       console.error('Error deleting address from MasterData:', error);
     });
 
+    // Fetch updated list of addresses
+    const { data: updatedAddresses } = await getAddresses();
     // Switch to the select-address view
     window.postMessage({ action: 'setDeliveryView', view: 'select-address' });
+
+    // Update the UI with the new list of addresses
+    const addressesHtml = updatedAddresses.map((addr) => AddressListing(addr)).join('');
+    if (document.getElementById('bash-address-list')) {
+      document.getElementById('bash-address-list').innerHTML = addressesHtml;
+    }
 
     showAlertBox('Address deleted successfully.');
   } catch (error) {
