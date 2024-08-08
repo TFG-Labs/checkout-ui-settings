d50517427d59d8cc12c6383e7a995cadf960e19c
Shaheen Karodia <karodia.shaheen@gmail.com>
2024-08-08 09:56:33 +0200

    Added Address Section form

--- checkout-ui-custom/src/partials/Deliver/AddAddressAutoCompleteManualForm.js
+++ checkout-ui-custom/src/partials/Deliver/AddAddressAutoCompleteManualForm.js
@@ -1,10 +1,13 @@
+import { AddressSectionHeading } from './FormComponents';
+
 // TODO
 const AddAddressAutoCompleteManualForm = (address) => {
   console.log('address', address);
   return /* html */ `
-    <div>
+   ${AddressSectionHeading('Delivery Details', 'address-search')}
+     <form id="bash--add-address-autocomplete-manual-form" method="post">
       <p>${JSON.stringify(address)}</p>
-    </div>
+    </form>
   `;
 };
 
