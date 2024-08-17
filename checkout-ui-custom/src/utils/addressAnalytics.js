675023a5909ccf6fe302b9e3162752bd2b98614b
Shaheen Karodia <karodia.shaheen@gmail.com>
2024-08-17 10:22:46 +0200

    added event types

---
+++ checkout-ui-custom/src/utils/addressAnalytics.js
@@ -0,0 +1,28 @@
+/// ////////////////////// ADDRESS ANALYTICS //////////////////////////
+
+// 1.  EVENT NAME: address_saved
+//     TRIGGER: On succesful API call for address creation/update //TODO on Address Update as well?
+//     PARAMETERS:
+//         - add_address_stage: profile | checkout
+//         - document_id: customers id  - TODO: verify if GTM automatically tracks
+//         - add_address_capture_method: auto_complete_google | manual_attempted_autocomplete_google | manual
+//         - add_address_method: search_for_an_address | edit_address | add_address_manually
+// TODO: how is there no reference to the address that was saved?
+// TODO:  what is the difference between add_address_capture_method and add_address_method?
+
+// 2.  EVENT NAME: add_address
+//     TRIGGER: On the initiation of the address flow ie we reached the view with the form to enter data
+//     PARAMETERS:
+//         - add_address_stage: profile | checkout
+//         - add_address_method: search_for_an_address | edit_address | add_address_manually
+//         - add_address_capture_method: auto_complete_google | manual_attempted_autocomplete_google | manual_entry
+//         - document_id: customers id  - TODO: verify if GTM automatically tracks
+// TODO:  how can  add_address_capture_method be valid if this is only triggered on getting to the view
+
+// 3.  EVENT NAME: add_address_error
+//     TRIGGER: On failure of API call for address creation/update, opposite call to the existing address_saved
+//     PARAMETERS:
+//         - add_address_stage: profile | checkout
+//         - add_address_method: search_for_an_address | edit_address | add_address_manually
+//         - add_address_capture_method: auto_complete_google | manual_attempted_autocomplete_google | manual_entry
+//         - document_id: customers id  - TODO: verify if GTM automatically tracks
