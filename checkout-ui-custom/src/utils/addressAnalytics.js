eac196f028c415fddb06fc13576dc9248149d8e3
Shaheen Karodia <karodia.shaheen@gmail.com>
2024-08-17 11:49:09 +0200

    Cleaned up address analytics events

--- checkout-ui-custom/src/utils/addressAnalytics.js
+++ checkout-ui-custom/src/utils/addressAnalytics.js
@@ -1,7 +1,8 @@
+/* eslint-disable camelcase */
 export const EVENT_NAME = {
-  ADDRESS_SAVED: 'address_saved',
-  ADD_ADDRESS: 'add_address',
-  ADD_ADDRESS_ERROR: 'add_address_error',
+  ADDRESS_SAVED: 'address_saved', // TRIGGER: On succesful API call for address creation/update
+  ADD_ADDRESS: 'add_address', // TRIGGER: On the initiation of the address flow i.e. we reached the view with the form to enter data
+  ADD_ADDRESS_ERROR: 'add_address_error', // TRIGGER: On failure of API call for address creation/update, opposite call to address_saved
 };
 
 export const PARAMETER = {
@@ -28,38 +29,40 @@ export const ADD_ADDRESS_METHOD = {
   ADD_ADDRESS_MANUALLY: 'add_address_manually',
 };
 
-/// ////////////////////// ADDRESS ANALYTICS //////////////////////////
+/**
+ * Tracks an address-related event and sends the relevant data for analytics purposes.
+ *
+ * @param {Object} params - The parameters for the event tracking.
+ * @param {EVENT_NAME[keyof typeof EVENT_NAME]} params.eventName - The name of the event to be tracked. Use one of the values from `EVENT_NAME` (e.g., `EVENT_NAME.ADDRESS_SAVED`).
+ * @param {ADD_ADDRESS_STAGE[keyof typeof ADD_ADDRESS_STAGE]} params.add_address_stage - The stage of the address addition process. Use one of the values from `ADD_ADDRESS_STAGE` (e.g., `ADD_ADDRESS_STAGE.PROFILE`).
+ * @param {ADD_ADDRESS_METHOD[keyof typeof ADD_ADDRESS_METHOD]} params.add_address_method - The method used to add the address. Use one of the values from `ADD_ADDRESS_METHOD` (e.g., `ADD_ADDRESS_METHOD.SEARCH_FOR_AN_ADDRESS`).
+ * @param {ADD_ADDRESS_CAPTURE_METHOD[keyof typeof ADD_ADDRESS_CAPTURE_METHOD]} params.add_address_capture_method - The method used to capture the address. Use one of the values from `ADD_ADDRESS_CAPTURE_METHOD` (e.g., `ADD_ADDRESS_CAPTURE_METHOD.AUTO_COMPLETE_GOOGLE`).
+ * @param {string} params.document_id - The unique identifier for the document associated with the address.
+ *
+ * @returns {void}
+ */
+export const trackAddressEvent = ({
+  eventName,
+  add_address_stage,
+  add_address_method,
+  add_address_capture_method,
+  document_id,
+}) => {
+  const payload = {
+    eventName,
+    add_address_stage,
+    add_address_method,
+    add_address_capture_method,
+    document_id,
+  };
 
-// 1.  EVENT NAME: address_saved
-//     TRIGGER: On succesful API call for address creation/update //TODO on Address Update as well?
-//     PARAMETERS:
-//         - add_address_stage: profile | checkout
-//         - document_id: customers id  - TODO: verify if GTM automatically tracks
-//         - add_address_capture_method: auto_complete_google | manual_attempted_autocomplete_google | manual
-//         - add_address_method: search_for_an_address | edit_address | add_address_manually
+  console.log('trackAddressEvent', payload);
+  // TODO decide how to pipe to GTM
+};
+
+// TODO        - document_id: customers id  - TODO: verify if GTM automatically tracks
+// TODO: likely we can only measure and check edit address related events after capture method
+// TODO: look at gaEvent in master
+// TODO: add_address  how can  add_address_capture_method be valid if this is only triggered on getting to the view
 // TODO: how is there no reference to the address that was saved?
 // TODO:  what is the difference between add_address_capture_method and add_address_method?
-export const trackAddressSaved = ({}) => {};
-
-// 2.  EVENT NAME: add_address
-//     TRIGGER: On the initiation of the address flow ie we reached the view with the form to enter data
-//     PARAMETERS:
-//         - add_address_stage: profile | checkout
-//         - add_address_method: search_for_an_address | edit_address | add_address_manually
-//         - add_address_capture_method: auto_complete_google | manual_attempted_autocomplete_google | manual_entry
-//         - document_id: customers id  - TODO: verify if GTM automatically tracks
-// TODO:  how can  add_address_capture_method be valid if this is only triggered on getting to the view
-export const trackAddAddress = ({}) => {};
-
-// 3.  EVENT NAME: add_address_error
-//     TRIGGER: On failure of API call for address creation/update, opposite call to the existing address_saved
-//     PARAMETERS:
-//         - add_address_stage: profile | checkout
-//         - add_address_method: search_for_an_address | edit_address | add_address_manually
-//         - add_address_capture_method: auto_complete_google | manual_attempted_autocomplete_google | manual_entry
-//         - document_id: customers id  - TODO: verify if GTM automatically tracks
-
-// TODO: likely we can only measure and check edit address related events after capture method
-export const trackAddAddressError = ({}) => {};
-
-export const trackAddressEvent = ({}) => {};
