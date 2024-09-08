f1b1698af1e25e60b7d351efdfbe5b166f134d4a
Shaheen Karodia <karodia.shaheen@gmail.com>
2024-09-08 16:49:00 +0200

    fixed locahost issues with api

--- checkout-ui-custom/src/utils/const.js
+++ checkout-ui-custom/src/utils/const.js
@@ -63,6 +63,7 @@ const getUrlApi = () => {
   if (prodHosts.includes(window.location.host)) {
     return 'https://store-api.www.bash.com/custom-api/';
   }
+  if (stageHosts.includes(window.location.host)) {
     return 'https://store-api.staging.tfglabs.dev/custom-api/';
   }
   return `${window.location.protocol}//${window.location.host}/custom-api/`;
